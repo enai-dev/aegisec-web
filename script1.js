00588f88eb0324d2b05ac22848ec898073c5ab3c
enai-dev <enai.galzacorta.kali@gmail.com>
2025-09-20 15:37:35 +0200

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,41 @@
+// ===== MENU HAMBURGUESA =====
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    navLinks.classList.toggle('active');
+});
+
+// ===== FORMULARIO CONTACTO =====
+const form = document.getElementById('contactForm');
+const formMessage = document.getElementById('formMessage');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault(); // evita recarga de página
+
+  const formData = {
+    nombre: form.name.value,
+    email: form.email.value,
+    mensaje: form.message.value
+  };
+
+  try {
+    const res = await fetch('http://localhost:3000/api/contactos', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    });
+
+    const data = await res.json();
+
+    if(data.id){
+      formMessage.textContent = "¡Mensaje enviado correctamente!";
+      form.reset();
+    } else {
+      formMessage.textContent = "Error al enviar el mensaje, inténtalo de nuevo.";
+    }
+  } catch (err) {
+    formMessage.textContent = "Error al conectar con el servidor.";
+  }
+});
